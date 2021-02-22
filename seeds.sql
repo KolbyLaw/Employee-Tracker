8cc7a20169eaffdd992a0b95fd30201c308ecdaf
Kolby Law <kolbytlaw@gmail.com>
2021-02-21 21:00:03 -0700

    Schema and Seed syntax fixes, added main JS logic.

--- seeds.sql
+++ seeds.sql
@@ -1,13 +1,38 @@
 -- Department Table Seeds
 INSERT INTO department (name)
-VALUES ("Sales"), ("Development"), ("Support"), ("Finance"), ("Legal");
+VALUE ("Sales");
+INSERT INTO department (name)
+VALUE ("Development");
+INSERT INTO department (name)
+VALUE ("Support");
+INSERT INTO department (name)
+VALUE ("Finance");
+INSERT INTO department (name)
+VALUE ("Legal");
 
 
 -- Role Table Seeds
 INSERT INTO role (title, salary, department_id)
-VALUE ("Sales Team Rep", 45000.00, 1), ("Full Stack Dev", 80000.00, 2), ("Customer Support Rep", 30000.00, 3), ("Accountant", 50000.00, 4), ("Lawyer", 120000.00, 5)
+VALUE ("Sales Team Rep", 45000.00, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Full Stack Dev", 80000.00, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Customer Support Rep", 30000.00, 3);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Accountant", 50000.00, 4);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Lawyer", 120000.00, 5);
+
 
 
 -- Employee Table Seeds
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUE ("Jack", "McSales", 1, 10), ("Jane", "McDev", 2, 20), ("Alyssa", "McSupport", 3, 30), ("Erin", "McFinance", 4, 40), ("John", "McLaw", 5, 50);
+VALUE ("Jack", "McSales", 1, 10);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Jane", "McDev", 2, 20);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Alyssa", "McSupport", 3, 30);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Erin", "McFinance", 4, 40);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("John", "McLaw", 5, 50);
---
+++ server.js
@@ -0,0 +1,222 @@
+// General Dependencies
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+const cTable = require("console.table");
+
+
+// General MYSQL Connection Info
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "MYSQLpassword@1",
+  database: "employee_database"
+});
+
+
+// MYSQL Connection
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected as ID " + connection.threadId);
+  startCommand();
+});
+
+
+// Primary Inquirer Prompt
+function startCommand() {
+    inquirer.prompt({
+        type: "list",
+        message: "What would you like to do?",
+        name: "choices",
+        choices: [
+        "View All Departments",
+        "View All Roles",
+        "View All Employees",
+        "Add a Department",
+        "Add a Role",
+        "Add an Employee",
+        "Update an Employee Role",
+        "Exit",
+        ]
+    }) .then(function(selectedChoice) {
+        console.log("You have selected: " + selectedChoice.choices);
+  
+        switch (selectedChoice.choices) {
+            case "View All Departments":
+                viewDepartments();
+                break;
+
+            case "View All Roles":
+                viewRoles();
+                break;
+
+            case "View All Employees":
+                viewEmployees();
+                break;
+
+            case "Add a Department":
+                addDepartment();
+                break;
+
+            case "Add a Role":
+                addRole();
+                break;
+
+            case "Add an Employee":
+                addEmployee();
+                break;
+
+            case "Update an Employee Role":
+                updateEmployeeRole();
+                break;
+
+            case "Exit":
+                exit();
+                break;
+        }
+    });
+};
+  
+
+// View All Departments
+function viewDepartments() {
+    let query = "SELECT * FROM department";
+    connection.query(query, function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        startCommand();
+    });
+};
+
+
+// View All Roles
+function viewRoles() {
+    let query = "SELECT * FROM role";
+    connection.query(query, function(err, res) {
+      if (err) throw err;
+      console.table(res);
+      startCommand();
+    });
+};
+
+
+// View All Employees
+function viewEmployees() {
+    let query = "SELECT * FROM employee";
+    connection.query(query, function(err, res) {
+      if (err) throw err;
+      console.table(res);
+      startCommand();
+    });
+};
+
+
+// Add a Department
+function addDepartment() {
+    inquirer.prompt({
+        type: "input",
+        message: "Please enter the name of the department: ",
+        name: "deptName"
+        })
+        .then(function(answer) {
+        connection.query("INSERT INTO department (name) VALUES (?)", [answer.deptName] , function(err, res) {
+            if (err) throw err;
+            console.table(res)
+            startCommand()
+        })
+    })
+};
+  
+
+// Add a Role
+function addRole() {
+    inquirer.prompt([
+        {
+          type: "input",
+          message: "Please enter the name of this role: ",
+          name: "roleName"
+        },
+        {
+          type: "input",
+          message: "Please enter the salary for this role: ",
+          name: "salaryTotal"
+        },
+        {
+          type: "input",
+          message: "Please enter the Department ID: ",
+          name: "deptID"
+        }
+        ])
+        .then(function(answer) {
+        connection.query("INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)", [answer.roleName, answer.salaryTotal, answer.deptID], function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        startCommand();
+        });
+    });
+};
+  
+
+// Add an Employee
+function addEmployee() {
+    inquirer.prompt([
+        {
+          type: "input",
+          message: "Please enter the employee's first name: ",
+          name: "employeeFirstName"
+        },
+        {
+          type: "input",
+          message: "Please enter the employee's last name: ",
+          name: "employeeLastName"
+        },
+        {
+          type: "input",
+          message: "Please enter the employee's Role ID: ",
+          name: "roleID"
+        },
+        {
+          type: "input",
+          message: "Please enter the Manager ID relative to this employee: ",
+          name: "managerID"
+        }
+        ])
+        .then(function(answer) {
+        connection.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)", [answer.employeeFirstName, answer.employeeLastName, answer.roleID, answer.managerID], function(err, res) {
+          if (err) throw err;
+          console.table(res);
+          startCommand();
+        });
+    });
+};
+  
+
+// Update an Employee Role  
+function updateEmployeeRole() {
+    inquirer.prompt([
+        {
+          type: "input",
+          message: "Select an employee to update their role: ",
+          name: "employeeUpdate"
+        },
+        {
+          type: "input",
+          message: "Please enter the Role ID: ",
+          name: "updateRole"
+        }
+        ])
+        .then(function(answer) { 
+        connection.query('UPDATE employee SET role_id=? WHERE first_name= ?',[answer.updateRole, answer.employeeUpdate],function(err, res) {
+          if (err) throw err;
+          console.table(res);
+          startCommand();
+        });
+    });
+};
+  
+
+// Exit
+function exit() {
+    connection.end();
+    process.exit();
+};
